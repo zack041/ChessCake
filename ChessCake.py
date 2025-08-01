30885fcb75a5004278b7dab21cb5468885894263
zack041 <zackyu041@gmail.com>
2025-08-01 18:43:05 +0800

    Update ChessCake.py

--- ChessCake.py
+++ ChessCake.py
@@ -44,6 +44,7 @@ class Engine:
         self.board = chess.Board(FEN) if FEN else chess.Board()
         self.z_table = tuple(random.getrandbits(64) for _ in range(781))
         self.t_table = {}
+        self.r_table = {}
         self.nodes = 0
         self.killer_moves = [[] for _ in range(100)]
         self.endgame = 0
@@ -192,12 +193,16 @@ class Engine:
                     break
             return min_eval
 
-    def minimax(self, alpha, beta, depth, ind, return_move=False,time_limit=None,start_time=None):
+    def minimax(self, alpha, beta, depth, ind, return_move,time_limit,start_time,max_depth):
         if time_limit is not None and start_time is not None:
             if time.time() - start_time > time_limit:
                 return None
         entry = self.t_table.get(ind)
         hash_move = None
+        if depth!=max_depth and self.r_table.get(ind)==1:
+            value = 0
+            self.t_table[ind] = t_entry(value, float('inf'))
+            return value
         if entry:
             if entry.depth >= depth:
                 if return_move:
@@ -213,7 +218,7 @@ class Engine:
 
         if self.board.is_stalemate() or self.board.is_insufficient_material() or self.board.is_fifty_moves():
             value = 0
-            self.t_table[ind] = t_entry(value, depth)
+            self.t_table[ind] = t_entry(value, float('inf'))
             return value
             
         if depth == 0:
@@ -234,7 +239,7 @@ class Engine:
             if depth >= 3 and not self.board.is_check() and self.endgame==0:
                 self.board.push(chess.Move.null())
                 new_ind = ind ^ self.z_table[780]
-                score = self.minimax(beta - 1, beta, depth - 2, new_ind, False, time_limit, start_time)
+                score = self.minimax(beta - 1, beta, depth - 2, new_ind, False, time_limit, start_time,max_depth)
                 self.board.pop()
                 if score is None:
                     return None
@@ -243,18 +248,19 @@ class Engine:
             max_eval = -float('inf')
             ct = 0
             for move in moves:
-                if depth >= 3 and ct >= 3 and not self.board.is_capture(move) and move not in self.killer_moves[depth] and not self.board.gives_check(move) and not self.board.is_check() and not move.promotion and self.endgame == 0:
+                nop = 1
+                if nop == 1 and depth >= 3 and ct >= 3 and not self.board.is_capture(move) and move not in self.killer_moves[depth] and not self.board.gives_check(move) and not self.board.is_check() and not move.promotion and self.endgame == 0:
                     new_ind = self.push(move, ind)
-                    #value = self.minimax(alpha, beta, depth - 2, new_ind, False, time_limit, start_time)
-                    value = self.minimax(alpha, beta, int(math.sqrt((len(moves) - ct) / len(moves)) * depth - 1), new_ind, False, time_limit, start_time)
+                    #value = self.minimax(alpha, beta, depth - 2, new_ind, False, time_limit, start_time,max_depth)
+                    value = self.minimax(alpha, beta, int(math.sqrt((len(moves) - ct) / len(moves)) * depth - 1), new_ind, False, time_limit, start_time,max_depth)
                     if value is None:
                         self.board.pop()
                         return None
                     if value > alpha:
-                        value = self.minimax(alpha, beta, depth - 1, new_ind, False, time_limit, start_time)
+                        value = self.minimax(alpha, beta, depth - 1, new_ind, False, time_limit, start_time,max_depth)
                 else:
                     new_ind = self.push(move, ind)
-                    value = self.minimax(alpha, beta, depth - 1, new_ind, False, time_limit, start_time)
+                    value = self.minimax(alpha, beta, depth - 1, new_ind, False, time_limit, start_time,max_depth)
                 self.board.pop()
                 if value is None:
                     return None
@@ -270,7 +276,6 @@ class Engine:
                             self.killer_moves[depth] = self.killer_moves[depth][:2]
                     break
                 ct += 1
-                
             self.t_table[ind] = t_entry(max_eval, depth, best_move)
             if return_move:
                 return [max_eval, best_move]
@@ -279,7 +284,7 @@ class Engine:
             if depth >= 3 and not self.board.is_check() and self.endgame==0:
                 self.board.push(chess.Move.null())
                 new_ind = ind ^ self.z_table[780]
-                score = self.minimax(alpha, alpha + 1, depth - 2, new_ind, False, time_limit, start_time)
+                score = self.minimax(alpha, alpha + 1, depth - 2, new_ind, False, time_limit, start_time,max_depth)
                 self.board.pop()
                 if score is None:
                     return None
@@ -290,16 +295,16 @@ class Engine:
             for move in moves:
                 if depth >= 3 and ct >= 3 and not self.board.is_capture(move) and move not in self.killer_moves[depth] and not self.board.gives_check(move) and not self.board.is_check() and not move.promotion and self.endgame==0:
                     new_ind = self.push(move, ind)
-                    #value = self.minimax(alpha, beta, depth - 2, new_ind, False, time_limit, start_time)
-                    value = self.minimax(alpha, beta, int(math.sqrt((len(moves) - ct) / len(moves)) * depth), new_ind, False, time_limit, start_time)
+                    #value = self.minimax(alpha, beta, depth - 2, new_ind, False, time_limit, start_time,max_depth)
+                    value = self.minimax(alpha, beta, int(math.sqrt((len(moves) - ct) / len(moves)) * depth), new_ind, False, time_limit, start_time,max_depth)
                     if value is None:
                         self.board.pop()
                         return None
                     if value < beta:
-                        value = self.minimax(alpha, beta, depth - 1, new_ind, False, time_limit, start_time)
+                        value = self.minimax(alpha, beta, depth - 1, new_ind, False, time_limit, start_time,max_depth)
                 else:
                     new_ind = self.push(move, ind)
-                    value = self.minimax(alpha, beta, depth - 1, new_ind, False, time_limit, start_time)
+                    value = self.minimax(alpha, beta, depth - 1, new_ind, False, time_limit, start_time,max_depth)
                 self.board.pop()
                 if value is None:
                     return None
@@ -328,50 +333,50 @@ class Engine:
         best_result = None
         for depth in range(1, max_depth + 1):
             self.nodes = 0
-            output = self.minimax(-999999999, 999999999, depth, ind, True, time_limit, start_time)
+            output = self.minimax(-999999999, 999999999, depth, ind, True, time_limit, start_time, depth)
             if output is None:
-                print("depth reached:", depth)
+                print("depth reached:", depth-1)
                 break
             best_result = output
         return best_result
-    
-    def play(time_limit,FEN=None):
-        engine = Engine(FEN)
-        ind = engine.zobrist_hash()
-        while True:
-            print(engine.board)
-            if engine.board.is_checkmate():
-                print("black win") if engine.board.turn else print("white win")
-                return
-            if engine.board.is_stalemate() or engine.board.is_insufficient_material() or engine.board.can_claim_draw():
-                print("draw")
-                return
-            depth = int(input("search depth"))
-            start = time.time()
-            b_pieces = 0
-            w_pieces = 0
-            if engine.endgame == 0:
-                for piece in engine.board.piece_map().values():
-                    if piece.color == chess.WHITE:
-                        w_pieces+=1
-                    else:
-                        b_pieces+=1
-                if min(w_pieces,b_pieces)<=4:
-                    engine.endgame = 1
-            #print(engine.minimax(-999999999, 999999999, depth, ind, True, time_limit, time.time()))
-            output = engine.iterative_deepening(depth,ind,time_limit)
-            end = time.time()
-            val = output[0]
-            move = output[1]
-            print("evaluation:", val)
-            print("best move:", move)
-            print("nodes searched:", engine.nodes)
-            print("time to search:", end-start)
-            makemove = chess.Move.from_uci(input("make move"))
-            if makemove in engine.board.legal_moves:
-                ind = engine.push(makemove,ind)
-            else:
-                print("move invalid")
+        
+def play(time_limit,FEN=None):
+    engine = Engine(FEN)
+    ind = engine.zobrist_hash()
+    while True:
+        print(engine.board)
+        if engine.board.is_checkmate():
+            print("black win") if engine.board.turn else print("white win")
+            return
+        if engine.board.is_stalemate() or engine.board.is_insufficient_material() or engine.board.can_claim_draw():
+            print("draw")
+            return
+        depth = int(input("search depth"))
+        start = time.time()
+        b_pieces = 0
+        w_pieces = 0
+        if engine.endgame == 0:
+            for piece in engine.board.piece_map().values():
+                if piece.color == chess.WHITE:
+                    w_pieces+=1
+                else:
+                    b_pieces+=1
+            if min(w_pieces,b_pieces)<=4:
+                engine.endgame = 1
+        #print(engine.minimax(-999999999, 999999999, depth, ind, True, time_limit, time.time()))
+        output = engine.iterative_deepening(depth,ind,time_limit)
+        end = time.time()
+        val = output[0]
+        move = output[1]
+        print("evaluation:", val)
+        print("best move:", move)
+        print("nodes searched:", engine.nodes)
+        print("time to search:", end-start)
+        makemove = chess.Move.from_uci(input("make move"))
+        if makemove in engine.board.legal_moves:
+            ind = engine.push(makemove,ind)
+        else:
+            print("move invalid")
 
 
 play(5)
